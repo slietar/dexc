c914904df51d6f9a5581c1e7e839da66bd4dcec0
Simon Liétar <simon@lietar.net>
2025-02-22 21:51:44 +0100

    Improve exception extraction

--- dexcp.py
+++ dexcp.py
@@ -1,22 +1,19 @@
 import ast
-from dataclasses import dataclass
+import functools
 import inspect
 import itertools
+import sys
+from dataclasses import dataclass
 from pathlib import Path
 from pprint import pprint
-import sys
 from types import CodeType, ModuleType, TracebackType
-from typing import Literal, Optional
+from typing import Literal, Optional, Sequence
 
-
-@dataclass(slots=True)
-class ExceptionItem:
-  message: str
 
 type AstNode = ast.Module | ast.expr | ast.stmt
 type ModuleKind = Literal['internal', 'std', 'lib', 'user']
 
-@dataclass(slots=True)
+@dataclass(frozen=True, slots=True)
 class ModuleItem:
   ast: Optional[ast.Module]
   instance: Optional[ModuleType]
@@ -24,19 +21,34 @@ class ModuleItem:
   name: str
   path: Path
 
-@dataclass(slots=True)
+@dataclass(eq=True, frozen=True, slots=True)
 class FrameItem:
   module: ModuleItem
   node: Optional[AstNode]
   reraise: bool
 
 
-def extract(start_exc: BaseException, /):
-  # List exceptions
-  # Re-raised exceptions form a single exception and cannot be detected at this stage.
+type ExceptionKind = Literal['base', 'cause', 'context']
 
+@dataclass(slots=True)
+class ExceptionItem:
+  children: 'Sequence[ExceptionChain]'
+  frames: Sequence[FrameItem]
+  instance: BaseException
+  kind: ExceptionKind
+
+@dataclass(slots=True)
+class ExceptionChain:
+  excs: Sequence[ExceptionItem]
+
+
+def extract(start_exc: BaseException, /):
+  return extract_exc_chain(start_exc)
+
+def extract_exc_chain(start_exc: BaseException, /):
   current_exc = start_exc
-  excs = list[tuple[BaseException, Literal['base', 'cause', 'context']]]()
+
+  excs = list[tuple[BaseException, ExceptionKind]]()
   excs.append((current_exc, 'base'))
 
   while True:
@@ -49,72 +61,72 @@ def extract(start_exc: BaseException, /):
     else:
       break
 
+  def map_exc(exc: BaseException, exc_kind: ExceptionKind):
+    return ExceptionItem(
+      children=([extract_exc_chain(exc) for exc in exc.exceptions] if isinstance(exc, BaseExceptionGroup) else []),
+      frames=extract_exc_frames(exc),
+      instance=exc,
+      kind=exc_kind,
+    )
 
-  for exc, exc_kind in excs:
-    # List frames
-
-    current_tb = exc.__traceback__
-    tbs = list[TracebackType]()
-
-    while current_tb:
-      tbs.append(current_tb)
-      current_tb = current_tb.tb_next
-
-
-    # Detect syntax error
-
-    frames = list[FrameItem]()
-    is_syntax_error = False # isinstance(exc, SyntaxError)
-
-    if is_syntax_error:
-      file.write(format_frame(
-        escape=escape,
-        frame_index=0,
-        func_name=Path(exc.filename).name,
-        prefix=prefix,
-        raw_path=exc.filename,
-        positions=(
-          exc.lineno,
-          exc.end_lineno,
-          (exc.offset - 1) if exc.offset is not None else None,
-          (
-            (exc.end_offset - 1) if exc.end_offset > 0 else exc.offset
-          ) if exc.end_offset is not None else None
-        ),
-        options=options
-      ))
+  return ExceptionChain([map_exc(exc, exc_kind) for exc, exc_kind in excs])
 
 
-    # Extract frames
 
-    for tb_index, tb in enumerate(reversed(tbs)):
-      frame = tb.tb_frame
-      frame_code = frame.f_code
-      raw_path = frame_code.co_filename
+def extract_exc_frames(exc: BaseException, /):
+  current_tb = exc.__traceback__
+  tbs = list[TracebackType]()
 
-      positions = next(
-        itertools.islice(frame_code.co_positions(), tb.tb_lasti // 2, None)
-      ) if tb.tb_lasti >= 0 else None
+  while current_tb:
+    tbs.append(current_tb)
+    current_tb = current_tb.tb_next
 
-      # print()
-      # print()
-      # print()
-      # print(frame_code)
-      # print(inspect.getmodule(frame_code))
-      # print()
-      # print()
-      # print()
-      # print(frame_code.co_name, inspect.getmodule(frame_code).__name__)
+  # Detect syntax error
 
-      frames.append(extract_frame(
-        code=frame_code,
-        frame_index=(tb_index + (1 if is_syntax_error else 0)),
-        func_name=frame_code.co_qualname,
-        raw_path=raw_path,
-        positions=positions,
-      ))
+  frames = list[FrameItem]()
+  is_syntax_error = False # isinstance(exc, SyntaxError)
 
-    pprint(frames)
+  # if is_syntax_error:
+  #   file.write(format_frame(
+  #     escape=escape,
+  #     frame_index=0,
+  #     func_name=Path(exc.filename).name,
+  #     prefix=prefix,
+  #     raw_path=exc.filename,
+  #     positions=(
+  #       exc.lineno,
+  #       exc.end_lineno,
+  #       (exc.offset - 1) if exc.offset is not None else None,
+  #       (
+  #         (exc.end_offset - 1) if exc.end_offset > 0 else exc.offset
+  #       ) if exc.end_offset is not None else None
+  #     ),
+  #     options=options
+  #   ))
+
+
+  # Extract frames
+
+  for tb_index, tb in enumerate(reversed(tbs)):
+    frame = tb.tb_frame
+    frame_code = frame.f_code
+    raw_path = frame_code.co_filename
+
+    positions = next(
+      itertools.islice(frame_code.co_positions(), tb.tb_lasti // 2, None)
+    ) if tb.tb_lasti >= 0 else None
+
+    frame = extract_frame(
+      code=frame_code,
+      frame_index=(tb_index + (1 if is_syntax_error else 0)),
+      func_name=frame_code.co_qualname,
+      raw_path=raw_path,
+      positions=positions,
+    )
+
+    frames.append(frame)
+
+  return frames
 
 
 
@@ -124,6 +136,10 @@ def extract_module_from_code(code: CodeType):
   if instance is None:
     return extract_module_from_path(Path(code.co_filename))
 
+  return extract_module_from_module(instance)
+
+@functools.cache
+def extract_module_from_module(instance: ModuleType):
   name = instance.__name__
   segments = name.split('.')
   path = Path(inspect.getfile(instance))
@@ -148,6 +164,7 @@ def extract_module_from_code(code: CodeType):
     path=path,
   )
 
+@functools.cache
 def extract_module_from_path(path: Path):
   name = inspect.getmodulename(path)
 
@@ -273,8 +290,6 @@ def identify_node(mod: ast.Module, line_start: int, line_end: int, col_start: Op
         new_candidates += (keyword.value for keyword in keywords)
       case ast.ClassDef(body=body):
         new_candidates += body
-      case ast.ExceptHandler(type, name, body):
-        new_candidates += [type, *body]
       case ast.Expr(value):
         new_candidates.append(value)
       case ast.AsyncFunctionDef(body=body) | ast.FunctionDef(body=body):
@@ -286,7 +301,11 @@ def identify_node(mod: ast.Module, line_start: int, line_end: int, col_start: Op
       case ast.For(target, iter, body, orelse, type_comment):
         new_candidates += [target, iter, *body, *orelse]
       case ast.Try(body, handlers, orelse, finalbody) | ast.TryStar(body, handlers, orelse, finalbody):
-        new_candidates += [*body, *handlers, *orelse, *finalbody]
+        new_candidates += [*body, *orelse, *finalbody]
+        new_candidates += [handler.type for handler in handlers]
+
+        for handler in handlers:
+          new_candidates += handler.body
       case ast.With(items, body, type_comment):
         new_candidates += (item.context_expr for item in items)
         new_candidates += body
