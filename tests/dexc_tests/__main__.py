1141032e38bafd6796163cdf70b5b13a432c5d18
Simon Liétar <simon@lietar.net>
2025-03-25 03:10:08 +0100

    Improve tests

--- tests/dexc_tests/__main__.py
+++ tests/dexc_tests/__main__.py
@@ -2,19 +2,24 @@ import sys
 from pathlib import Path
 
 import dexc
-
 import dexc.install
 from dexc.options import Options
 
 
-for file_path in sorted(Path(__file__).parent.glob('t*.py')):
-  if '_' in file_path.stem:
-    continue
-
-  print(f'-- {file_path.stem} {'-' * 80}')
-
+def run_test(module_name: str):
   try:
-    mod = __import__(f'dexc_tests.{file_path.stem}', fromlist=('a'))
+    mod = __import__(module_name, fromlist=('a'))
     mod.main()
   except Exception as e:
     dexc.install.dump(e, sys.stdout, Options())
+
+
+if len(sys.argv) > 1:
+  run_test(f'dexc_tests.{sys.argv[1]}')
+else:
+  for file_path in sorted(Path(__file__).parent.glob('t*.py')):
+    if '_' in file_path.stem:
+      continue
+
+    print(f'-- {file_path.stem} {'-' * 80}')
+    run_test(f'dexc_tests.{file_path.stem}')
--- tests/dexc_tests/t05.py
+++ tests/dexc_tests/t05.py
@@ -2,16 +2,50 @@ def main():
   def produce_exc(name: str):
     try:
       raise Exception(name)
+
+
+
+
     except Exception as e:
       return e
+
+  def a(x: int):
+    if x == 0:
+      raise Exception('Done')
+
+    b(x)
+
+  def b(x: int):
+    return a(x - 1)
+
+  def c(x: int):
+    try:
+      b(7)
+    except Exception as e:
+      return e
+
+  def d():
+    raise ExceptionGroup('Group3', [
+      produce_exc('F'),
+      produce_exc('G'),
+    ])
+
+  def e():
+    try:
+      d()
+    except Exception as e:
+      return e
+
 
   raise ExceptionGroup('Group', [
     produce_exc('A'),
     produce_exc('B'),
     Exception('C'),
+    c(4),
     ExceptionGroup('Group2', [
       produce_exc('D'),
       produce_exc('E')
     ]),
     Exception('D'),
+    e(),
   ])
